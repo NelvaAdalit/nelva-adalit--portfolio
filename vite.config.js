aec160780f98afd4921875b8b7b09f0c5d11c962
NelvaAdalit <moraadalid378@gmail.com>
2026-07-10 11:03:59 -0400

    button

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
@@ -7,6 +8,13 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu-demo.html')
+      }
+    }
   }
 });
+
